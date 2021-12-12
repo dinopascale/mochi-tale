5111918f997c88a60fba80a49cbfce3de4d59745
dpascale <dino.pascale@gmail.com>
2021-12-12 15:34:10 +0100

    [TAILWIND]: Remove `purge` option from config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  purge: [
-    // Use *.tsx if using TypeScript
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
